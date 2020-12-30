7fc3c0fbe17fc465e116446af4fb3c3a9dc600a5
Arvin <gnimoaix857@gmail.com>
2020-12-30 22:29:59 +0700

    Remove character that caused syntax error

--- lib/src/services/auth.dart
+++ lib/src/services/auth.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/material.dart.';
+import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
